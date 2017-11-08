16547a4e3c27d4cd6d691ddc6ba2bd3ede72d3ac
Artem Govorov <artem.govorov@gmail.com>
2017-11-08 15:19:18 +1000

    Update wallaby.js

--- wallaby.js
+++ wallaby.js
@@ -6,14 +6,10 @@ module.exports = () => {
       tests: [
         'tests/**/*.spec.ts',
       ],
-      compilers: {
-        '**/*.ts': wallaby.compilers.typeScript({
-          // TypeScript compiler specific options
-          // https://github.com/Microsoft/TypeScript/wiki/Compiler-Options
-          // (no need to duplicate tsconfig.json, if you have it, it'll be automatically used)
-        })
+      env: {
+        type: 'node'
       },
-      debug: true
+      debug: true,
+      testFramework: 'jest'
     };
   };
-  
