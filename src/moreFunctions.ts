db64c5c0a4f44d16a8042c89a048c27e9f1b3785
Damien Sawyer <damiensawyer@gmail.com>
2017-11-08 22:21:49 +1100

    wallaby working

---
+++ src/moreFunctions.ts
@@ -0,0 +1,8 @@
+export default function() {
+  return {
+    Multiply: (first: number, second: number) => {
+       /*$ first*/
+      return first * second; /*?. */
+    },
+  };
+}
--- tests/function.spec.ts
+++ tests/function.spec.ts
@@ -1,7 +1,13 @@
+import otherFunctions from "../src/moreFunctions";
 import { Add, Subtract } from "../src/myfunctions";
+
 describe("test model", () => {
   it("should match numbers", () => {
     expect(1).toBe(1);
+  });
+
+  it("should multiply", () => {
+    expect(otherFunctions().Multiply(2, 3)).toBe(6);
   });
 
   it("should behave...", () => {
